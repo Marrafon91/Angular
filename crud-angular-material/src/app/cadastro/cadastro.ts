d9c93b2aa3ef2bad448b87a3485cff9aa0923632
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-10-21 14:33:31 -0300

    Iniciando o Formulário de Clientes

--- crud-angular-material/src/app/cadastro/cadastro.ts
+++ crud-angular-material/src/app/cadastro/cadastro.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatCardModule } from '@angular/material/card'
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule, MatInput } from '@angular/material/input'
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule, MatInput],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss'
 })
