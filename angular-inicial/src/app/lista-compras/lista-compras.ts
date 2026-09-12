588fb6d4a73f2a7f8f0de0e6051daae0815e3269
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-12 16:08:27 -0300

    terminando a lista

--- angular-inicial/src/app/lista-compras/lista-compras.ts
+++ angular-inicial/src/app/lista-compras/lista-compras.ts
@@ -1,25 +1,39 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 import { ItemLista } from './itemlista';
 
 @Component({
-  imports: [FormsModule],
   selector: 'app-lista-compras',
-  styleUrl: './lista-compras.css',
+  imports: [FormsModule, NgClass],
   templateUrl: './lista-compras.html',
+  styleUrl: './lista-compras.css',
 })
 export class ListaCompras {
   item: string = '';
   lista: ItemLista[] = [];
+  erro: string = '';
 
   adiconarItem() {
+    // Verifica se o campo está vazio ou contém apenas espaços
+    if (!this.item.trim()) {
+      this.erro = 'Não é possível adicionar um item em branco.';
+      return;
+    }
+
     let itemLista = new ItemLista();
 
     itemLista.id = this.lista.length + 1;
-    itemLista.nome = this.item;
+    itemLista.nome = this.item.trim();
 
     this.lista.push(itemLista);
-    
+
     this.item = '';
+    this.erro = '';
+  }
+
+  limparLista() {
+    this.lista = [];
+    this.erro = '';
   }
 }
