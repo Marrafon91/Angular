5e7a4d487cb87b729fcbda69d8e51858ed9c3d12
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-12 15:28:27 -0300

    Utilizando o @for e o ngFor para renderizar componentes de lista

--- angular-inicial/src/app/lista-compras/lista-compras.ts
+++ angular-inicial/src/app/lista-compras/lista-compras.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-lista-compras',
   styleUrl: './lista-compras.css',
   templateUrl: './lista-compras.html',
 })
-export class ListaCompras {}
+export class ListaCompras {
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adiconarItem() {
+    let itemLista = new ItemLista();
+
+    itemLista.id = this.lista.length + 1;
+    itemLista.nome = this.item;
+
+    this.lista.push(itemLista);
+    
+    this.item = '';
+  }
+}
