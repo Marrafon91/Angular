d1ab83d1d4eaf096489798b6472862c5d6b1f2f3
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-13 19:32:21 -0300

    Router Link e Material Icons

--- crud-angular/src/app/app.ts
+++ crud-angular/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
