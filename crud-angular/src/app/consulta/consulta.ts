85f8bf36e55a3a7273ba638447f6a84eda6042ac
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-13 19:21:06 -0300

    Criando um toolbar e configurando navegação entre páginas

---
+++ crud-angular/src/app/consulta/consulta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-consulta',
+  styleUrl: './consulta.css',
+  templateUrl: './consulta.html',
+})
+export class Consulta {}
