2f13d5b2aadafe71dfe5583516cc5ac662543cc7
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-14 19:04:27 -0300

    Finalizando a parte visual do formulário

--- crud-angular/src/app/cadastro/cadastro.ts
+++ crud-angular/src/app/cadastro/cadastro.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
