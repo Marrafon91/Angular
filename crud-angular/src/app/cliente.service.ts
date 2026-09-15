7a24bbb9ede3ace15e754c7a4682ebab39c16f8a
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-15 19:25:02 -0300

    Criando o repositorio de clientes para persistir os dados

--- crud-angular/src/app/cliente.service.ts
+++ crud-angular/src/app/cliente.service.ts
@@ -1,9 +1,30 @@
 import { Service } from '@angular/core';
 import { Cliente } from './cadastro/cliente';
+import { C } from '@angular/cdk/keycodes';
 
 @Service()
 export class ClienteService {
+  static REPO_CLIENTES = '_CLIENTES';
+
   salvar(cliente: Cliente) {
     console.log(cliente);
+  }
+
+  obterStorage(): Cliente[] {
+    const repositorioClientes = localStorage.getItem(
+      ClienteService.REPO_CLIENTES,
+    );
+
+    if (repositorioClientes) {
+      const clientes: Cliente[] = JSON.parse(repositorioClientes);
+      return clientes;
+    }
+
+    const clientes: Cliente[] = [];
+    localStorage.setItem(
+      ClienteService.REPO_CLIENTES,
+      JSON.stringify(clientes),
+    );
+    return clientes;
   }
 }
