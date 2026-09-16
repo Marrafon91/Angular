8167e0b924d4c6cb87a43030c473533100bfb956
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-16 06:58:19 -0300

    Criando o formulário de pesquisa

--- crud-angular/src/app/consulta/consulta.ts
+++ crud-angular/src/app/consulta/consulta.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    MatButtonModule,
+  ],
   selector: 'app-consulta',
   styleUrl: './consulta.css',
   templateUrl: './consulta.html',
